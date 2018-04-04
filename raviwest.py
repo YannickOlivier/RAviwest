111a072c2f01676d7c1957f7af4b2632d07f9306
YannickOlivier <yannick.olivier2@gmail.com>
2018-04-04 12:39:17 +0200

    CX_Freeze & clean code

--- raviwest.py
+++ raviwest.py
@@ -1,25 +1,48 @@
-## Simple application to reboot Aviwest Streamhub services and server
-## Sorry, it's my first app in Python
+#  This file is part of Raviwest.
+#  Simple application to reboot Aviwest Streamhub services and server.
+#
+#  Raviwest is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  Raviwest is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with Raviwest.  If not, see <http://www.gnu.org/licenses/>.
+#
+#  Created by Yannick Olivier in 2018. France 3 Centre Val de Loire.
+#
+#  Sorry, it's my first app in Python
 
 ## Modules
 from tkinter import Tk, Label, Button, Message, Canvas, Frame, Text, Scrollbar, StringVar, Entry, ttk, Place
-import paramiko, socket, subprocess, json
+import paramiko, socket, subprocess, json, os
 
 ## VAR
 HOST_IP = ''
 HOST_PORT = ''
 HOST_LOGIN = ''
 HOST_PASSWORD = ''
-
-## Parameters for StringVar
-#parametersIP = StringVar()
-
+COMMAND_SERVICES = 'pwd'
+COMMAND_REBOOT = 'pwd'
 
 ## Constructor
 client = paramiko.SSHClient()
 maFenetre = Tk()
 
 ## My functions 
+def jsonCreation():
+    if os.path.isfile('config.json'):
+        print("Fichier OK : pas de création")
+    else:
+        print("Fichier NOK : création")
+        data = {'ip': '', 'port': '', 'login': '', 'password': '', 'services': COMMAND_SERVICES, 'reboot': COMMAND_REBOOT}
+        json.dump(data, open('config.json', 'w'), indent=4)
+
 def ping():
     response = subprocess.call(['ping', '-w', '1', '-n', '1', HOST_IP])
     if response == 0:
@@ -27,11 +50,14 @@ def ping():
     else:
         pass
 
-def connectSSH():
-    print("ConnectSSH")
-    client.load_system_host_keys()
-    client.set_missing_host_key_policy(paramiko.AutoAddPolicy)
-    client.connect(HOST_IP, HOST_PORT, HOST_LOGIN, HOST_PASSWORD)
+def connectSSH(HOST_IP, HOST_PORT, HOST_LOGIN, HOST_PASSWORD):
+    try:
+        print("ConnectSSH")
+        client.load_system_host_keys()
+        client.set_missing_host_key_policy(paramiko.AutoAddPolicy)
+        client.connect(HOST_IP, HOST_PORT, HOST_LOGIN, HOST_PASSWORD)
+    except:
+        print("Le serveur ne répond pas")
 
 def closeSSH():
     client.close()
@@ -42,12 +68,16 @@ def commandSSH(command):
 
 def rebootServices():
     pass
-    connectSSH()
+    connectSSH(textIP.get(), textPORT.get(), textLOGIN.get(), textPASSWORD.get())
     commandSSH("pwd; pwd")
     closeSSH()
 
 def rebootServeur():
-    pass
+    print("Reboot demandé")
+    print(textIP.get())
+    connectSSH(textIP.get(), textPORT.get(), textLOGIN.get(), textPASSWORD.get())
+    commandSSH(COMMAND_REBOOT)
+    closeSSH()
 
 def configuration():
     conf = json.load(open('config.json', 'rb'))
@@ -57,21 +87,16 @@ def configuration():
     HOST_PASSWORD = conf['password']
     return HOST_IP, HOST_PORT, HOST_LOGIN, HOST_PASSWORD
 
-def sauvegarde(textIP):
+def sauvegarde():
     print("Sauvegarde")    
-    print("salu", textIP)
-    data = {"ip": textIP, 'toto2': 'toto2'}
-    json.dump(data, open('toto.json', 'w') )
+    data = {'ip': textIP.get(), 'port': textPORT.get(), 'login': textLOGIN.get(), 'password': textPASSWORD.get(), 'services': COMMAND_SERVICES, 'reboot': COMMAND_REBOOT}
+    json.dump(data, open('config.json', 'w'), indent=4)
 
 ## Tkinter Général 
 maFenetre.iconbitmap("logo.ico")
-maFenetre.title('Raviwest 0.1')
-maFenetre.geometry('220x300')
+maFenetre.title('Raviwest 0.2')
+maFenetre.geometry('200x280+600+300')
 maFenetre.resizable(width='False', height='False')
-#maFenetre.rowconfigure(0, weight='1')
-#maFenetre.columnconfigure(0, weight='1')
-
-
 
 ## Tkinter Text : 
 def tkinterText(HOST_IP, HOST_PORT, HOST_LOGIN, HOST_PASSWORD):
@@ -86,8 +111,11 @@ def tkinterText(HOST_IP, HOST_PORT, HOST_LOGIN, HOST_PASSWORD):
     textPASSWORD.set(HOST_PASSWORD)
     return textIP, textPORT, textLOGIN, textPASSWORD
 
+def action():
+    print("action")
+
 ## Tkinter Constructeur :
-def tkinterConstructeur(textIP, textPORT, textLOGIN, textPASSWORD):
+def tkinterConstructeur():
     text1Label = Label(maFenetre, text='Choisir le type de reboot désiré :', anchor='center').grid(column='0', row='0', padx='10', pady='5')
     boutonServices = Button(maFenetre, text ='Redémarrer les services', command=rebootServices).grid(column='0', row='1', padx='10', pady='5', sticky='nesw')
     boutonServeur = Button(maFenetre, text ='Redémarrer le serveur', command=rebootServeur).grid(column='0', row='2', padx='10', pady='5', sticky='nesw')
@@ -101,11 +129,11 @@ def tkinterConstructeur(textIP, textPORT, textLOGIN, textPASSWORD):
     entryLOGIN = Entry(maFenetre, textvariable=textLOGIN).grid(column='0', row='7', columnspan='2', rowspan='1', padx='0', pady='1', sticky='e')
     textPASSWORDLabel = Label(maFenetre, text='PASSWORD :').grid(column='0', row='8', padx='0', pady='1', sticky='w')
     entryPASSWORD = Entry(maFenetre, textvariable=textPASSWORD, show="*").grid(column='0', row='8', columnspan='2', rowspan='1', padx='0', pady='1', sticky='e')
-    boutonConf = Button(maFenetre, text ='Valider', command=sauvegarde(textIP.get())).grid(column='0', row='9', padx='10', pady='5', sticky='nesw')
+    boutonConf = Button(maFenetre, text ='Valider', command=sauvegarde).grid(column='0', row='9', padx='10', pady='5', sticky='nesw')
 
-#textIP.get())
 ## Tkinter MainLoop
+jsonCreation()
 HOST_IP, HOST_PORT, HOST_LOGIN, HOST_PASSWORD = configuration()
 textIP, textPORT, textLOGIN, textPASSWORD = tkinterText(HOST_IP, HOST_PORT, HOST_LOGIN, HOST_PASSWORD)
-tkinterConstructeur(textIP, textPORT, textLOGIN, textPASSWORD)
+tkinterConstructeur()
 maFenetre.mainloop()
---
+++ setup.py
@@ -0,0 +1,20 @@
+from cx_Freeze import setup, Executable
+import os
+
+os.environ['TCL_LIBRARY'] = "C:\\Users\\yannick.olivier\\AppData\\Local\\Programs\\Python\\Python36\\tcl\\tcl8.6"
+os.environ['TK_LIBRARY'] = "C:\\Users\\yannick.olivier\\AppData\\Local\\Programs\\Python\\Python36\\tcl\\tk8.6"
+
+includes = []
+include_files = [r"C:\\Users\\yannick.olivier\\AppData\\Local\\Programs\\Python\\Python36\DLLs\\tcl86t.dll",
+                 r"C:\\Users\\yannick.olivier\\AppData\\Local\\Programs\\Python\\Python36\DLLs\\tk86t.dll",
+                 r"C:\\appli_ftv\\_Yannick\\Github\\RAviwest\\logo.ico"]
+packages = ['paramiko', 'cffi', 'cryptography']
+
+
+setup(
+    name = "RAviwest",
+    options = {"build_exe": {"includes": includes, "include_files": include_files, "packages": packages}},
+    version = "0.2",
+    description = 'A simple app to reboot Aviwest StreamHub',
+    executables = [Executable(script="raviwest.py", base='Win32GUI', icon="logo.ico")]
+)
